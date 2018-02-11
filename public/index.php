965f985991f45bb686ecb565423d294ed5a2e127
hthofficial <hthofficial2017@gmail.com>
2018-02-12 01:59:55 +0530

    improvement

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
  use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
-require_once '../include/dboperations.php';
+require_once '../includes/dboperations.php';
 $app= new \Slim\App(['settings'=>['displayErrorDetails'=>true]]);
 $app->get('/test',function(Request $req,Response $res){
 	    $result="it works.app is updated";
