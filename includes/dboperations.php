ff7938bc0f70653f4887d6a763eebdb6621cee2e
hthofficial <hthofficial2017@gmail.com>
2018-02-12 00:31:33 +0530

    operations created

---
+++ includes/dboperations.php
@@ -0,0 +1,80 @@
+<?php
+class DbOperation
+{
+  private $con;
+  function __construct()
+  {
+    require_once dirname(__FILE__).'/dbconnect.php';
+	$db=new DbConnect();
+	$this->con=$db->connect();
+  }
+   function getProjects()
+  {
+      $stmt=$this->con->prepare("SELECT * FROM projects WHERE project_prior = 'featured'");
+	  $stmt->execute();
+	  $stmt->bind_result($id,$name,$city,$type,$possesion,$price,$address);
+	  $property=array();
+	  while($stmt->fetch())
+	  {
+	    $temp=array();
+		$temp['proId']=$id;
+		$temp['Name']=$name;
+		$temp['City']=$city;
+		$temp['Type']=$type;
+		$temp['Possesion']=$possesion;
+		$temp['Type']=$type;
+		$temp['Address']=$address;
+		array_push($propety,$temp)
+		
+	  }
+	  return $property;
+  }  
+   function getProjectsbyCity($city)
+    {
+		$stmt=$this->con->prepare("SELECT * FROM projects WHERE project_city=?");
+		$stmt->bind_param("s",$city);
+		$stmt->execute();
+		$stmt->bind_result($id,$name,$city,$type,$possesion,$price,$address);
+		$property=array();
+		while($stmt->fetch())
+		{
+			$temp=array();
+			$temp['proId']=$id;
+		$temp['Name']=$name;
+		$temp['City']=$city;
+		$temp['Type']=$type;
+		$temp['Possesion']=$possesion;
+		$temp['Type']=$type;
+		$temp['Address']=$address;
+		array_push($propety,$temp);
+			
+		}
+		
+		 return $property;
+	}   
+    function getCities()
+	{
+	
+	        $stmt=$this->con->prepare("SELECT DISTINCT project_city FROM projects");
+			$stmt->execute();
+			$stmt->bind_result($city);
+			$cities=array();
+			while($stmt->fetch())
+			{
+				$temp=array();
+				$temp['City']=$city;
+				array_push($cities,$temp);
+			}
+			return $cities;
+	}
+	function getRentables()
+	{
+	  
+	}
+	function getSaleables()
+	{
+	  
+	}
+}
+
+?>
