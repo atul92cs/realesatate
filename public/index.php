49697f7d2ccffdeae11bae164619a15276ca2c01
hthofficial <hthofficial2017@gmail.com>
2018-02-12 00:47:51 +0530

    index work started

--- public/index.php
+++ public/index.php
@@ -1,30 +1,12 @@
 <?php
-if (PHP_SAPI == 'cli-server') {
-    // To help the built-in PHP dev server, check if the request was actually for
-    // something which should probably be served as a static file
-    $url  = parse_url($_SERVER['REQUEST_URI']);
-    $file = __DIR__ . $url['path'];
-    if (is_file($file)) {
-        return false;
-    }
-}
+use \Psr\Http\Message\ServerRequestInterface as Request;
+ use \Psr\Http\Message\ResponseInterface as Response;
 
-require __DIR__ . '/../vendor/autoload.php';
-
-session_start();
-
-// Instantiate the app
-$settings = require __DIR__ . '/../src/settings.php';
-$app = new \Slim\App($settings);
-
-// Set up dependencies
-require __DIR__ . '/../src/dependencies.php';
-
-// Register middleware
-require __DIR__ . '/../src/middleware.php';
-
-// Register routes
-require __DIR__ . '/../src/routes.php';
-
-// Run app
+require '../vendor/autoload.php';
+require_once '../include/dboperations.php';
+$app= new \Slim\App(['settings'=>['displayErrorDetails'=>true]]);
+$app->get('/test',function(Request $req,Response $res){
+	    $result="it works.app is updated";
+		$res->getBody()->write(json_encode(array($result)));
+});
 $app->run();
